b06c9f855407c1640989a2c0e3dd0b219a9726c9
kadavathpriyanka <kadavathpriyanka111@gmail.com>
2026-06-19 23:11:21 +0530

    toggle added!

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,3 +1,20 @@
-//alert("hello from JS !");
+let isMale = true;
 
-console.log("hello from js!");
+function toggleCard()
+{
+    const img = document.querySelector(".card img");
+    const name = document.querySelector(".card h2");
+    const gender = document.querySelector(".card p");
+
+    if ( isMale ) {
+        img.src = "../images/woman.png"; // female image
+        name.textContent = "Priyanka";
+        gender.textContent = "Female";
+    } else {
+        img.src = "../images/john.png"; // male image
+        name.textContent = "John";
+        gender.textContent = "Male";
+    }
+
+    isMale = !isMale;
+}
